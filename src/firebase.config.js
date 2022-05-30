0fa3ead7576f39f1b370d4da25fefa3568d001c4
ybrefodlavir <rivaldoferby69@gmail.com>
2022-05-30 11:57:04 +0700

    setup firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyA95tysm0t33K9JjFJvQbzreLke3-Viuj8",
+  authDomain: "restauranto-4867d.firebaseapp.com",
+  projectId: "restauranto-4867d",
+  storageBucket: "restauranto-4867d.appspot.com",
+  messagingSenderId: "609933775984",
+  appId: "1:609933775984:web:c67313f52db46369e4d0b5",
+  measurementId: "G-E5JKS5GBSB",
+};
