6b21191ab84cee99b2006b884900e2101d244115
Fikkie007 <fikkieakbar48@gmail.com>
2022-06-28 01:59:05 +0700

    sign out button fix

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useNavigate, useLocation} from "react-router-dom";
+
 
 import "../pages/Style/Navbar.css";
 
+import { firebaseauth } from "../firebase/FirebaseAuth";
+import {
+  signOut,
+} from "firebase/auth";
+
 const NavbarComponent = () => {
+  let navigate = useNavigate();
+  let location = useLocation();
+  let { from } = location.state || { from: { pathname: "/" } };
+  const logout = async () => {
+    await signOut(firebaseauth);
+    navigate("/", { from });
+  };
   return (
     <Navbar variant="dark" expand="lg">
       <Container>
@@ -14,7 +28,7 @@ const NavbarComponent = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Link to="/" className="SignOut">Sign Out</Link>
+            <button onClick={logout} className="SignOut">Sign Out</button>
             
             
           </Nav>
