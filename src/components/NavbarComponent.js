86d31a25862f4a70c25728b14fb3dbbced95bc70
Fikkie007 <fikkieakbar48@gmail.com>
2022-06-28 00:37:12 +0700

    sign out method

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+import "../pages/Style/Navbar.css";
+
 const NavbarComponent = () => {
   return (
     <Navbar variant="dark" expand="lg">
@@ -12,19 +14,9 @@ const NavbarComponent = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Link to="/">Login</Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Link to="/" className="SignOut">Sign Out</Link>
+            
+            
           </Nav>
         </Navbar.Collapse>
       </Container>
